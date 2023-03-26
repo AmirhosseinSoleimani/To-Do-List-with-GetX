be3a12ed412efdbcf46005f4a8d88892b64296a8
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-03-26 10:51:45 +0430

    Add HexColor in extension

--- lib/core/utils/extension.dart
+++ lib/core/utils/extension.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 extension PercentSize on double{
@@ -8,4 +8,14 @@ extension PercentSize on double{
 
 extension ResponsiveText on double{
   double get sp => Get.width / 100 * (this / 3);
+}
+
+extension HexColor on Color{
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll('#', '');
+    if (hexColorString.length == 6) {
+      hexColorString = 'FF$hexColorString'; // 8 char with opacity 100%
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
 }
