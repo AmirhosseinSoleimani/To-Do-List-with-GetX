04b3bac2a5ad990c09745d40ca165b765f93bbd6
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-03-26 11:07:20 +0430

    Apply GetStorage init in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'modules/home/view.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+
+void main() async{
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
