e7199526b63e82817095f26fe4f9ba29cd2a509a
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-03-26 11:17:37 +0430

    Create StorageService class in services and Apply in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_with_getx/data/services/storage/services.dart';
 import 'modules/home/view.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -6,6 +7,7 @@ import 'package:get_storage/get_storage.dart';
 
 void main() async{
   await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
 
