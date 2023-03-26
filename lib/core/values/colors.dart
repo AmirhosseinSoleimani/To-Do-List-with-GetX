35aecf0988914afc3d937231cd5a8d17f6f1038f
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-03-26 10:57:58 +0430

    Add ColorManager Class in values

---
+++ lib/core/values/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorManager{
+  static Color purple = const Color(0xFF756BFC);
+  static Color pink = const Color(0xFF756BFC);
+  static Color deepPink = const Color(0xFFFA63C6);
+  static Color green = const Color(0xFF41CF9F);
+  static Color yellow = const Color(0xFFEEC38E);
+  static Color lightBlue = const Color(0xFF42A5F5);
+  static Color blue = const Color(0xFF2B60E6);
+}
