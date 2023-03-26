5a5e7a2af0d14c0cb3583e44fe940c5fe7d9f534
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-03-26 06:41:55 +0430

    Create extension and Add PercentSize in extension

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'modules/home/view.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       title: 'Task Manager using GetX',
       home: HomePage(),
     );
