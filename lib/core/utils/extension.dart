d794b45c8ce6ebc567eb4bf477299dbdbd23398a
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-03-26 10:35:17 +0430

    Add ResponsiveText in extension

--- lib/core/utils/extension.dart
+++ lib/core/utils/extension.dart
@@ -5,3 +5,7 @@ extension PercentSize on double{
   double get hp => (Get.height * (this / 100));
   double get wp => (Get.width * (this / 100));
 }
+
+extension ResponsiveText on double{
+  double get sp => Get.width / 100 * (this / 3);
+}
