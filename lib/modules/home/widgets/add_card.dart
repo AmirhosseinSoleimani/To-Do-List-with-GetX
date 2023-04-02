b6b3ceae759df0e21374c4942bc7fa101efc757b
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-04-02 23:49:56 +0430

    Create AddCard class in add_card.dart in Widgets

---
+++ lib/modules/home/widgets/add_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:task_manager_with_getx/core/utils/extension.dart';
+import 'package:task_manager_with_getx/modules/home/controller.dart';
+import 'package:task_manager_with_getx/modules/home/widgets/icons.dart';
+import 'package:dotted_border/dotted_border.dart';
+
+class AddCard extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  AddCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final icons = getIcons();
+    var squaredWidth = Get.width - 12.0.wp;
+    return Container(
+      width: squaredWidth / 2,
+      height: squaredWidth / 2,
+      margin: EdgeInsets.all(3.0.wp),
+      child: InkWell(
+        onTap: () {},
+        child: DottedBorder(
+          color: Colors.grey[400]!,
+          dashPattern: const [8, 4],
+          child: Center(
+            child: Icon(
+                Icons.add,
+                size: 10.0.wp,
+                color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
