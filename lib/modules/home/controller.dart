b9cab7dcc117b36e598d2cf0b5158e7c527ced77
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-04-02 23:33:10 +0430

    Add onInit and onClose function to HomeController in controller.dart

--- lib/modules/home/controller.dart
+++ lib/modules/home/controller.dart
@@ -1,10 +1,26 @@
 import 'package:get/get.dart';
 import 'package:task_manager_with_getx/data/services/storage/repository.dart';
+import '../../data/models/task.dart';
 
 class HomeController extends GetxController {
   TaskRepository taskRepository;
   HomeController({
     required this.taskRepository,
   });
+
+  final tasks = <Task>[].obs;
+
+  @override
+  void onInit(){
+    super.onInit();
+    tasks.assignAll(taskRepository.readTask());
+    ever(tasks, (_) => taskRepository.writeTasks(tasks));
+  }
+
+  @override
+  void onClose(){
+    super.onClose();
+
+  }
 
 }
