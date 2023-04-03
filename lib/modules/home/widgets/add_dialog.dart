a19f19239ff9c2d3c346c0b0c82a64d0e1670acd
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-04-03 20:37:45 +0430

    Add List all the task for selection to toAddDialog

--- lib/modules/home/widgets/add_dialog.dart
+++ lib/modules/home/widgets/add_dialog.dart
@@ -22,6 +22,8 @@ class AddDialog extends StatelessWidget {
                   IconButton(
                       onPressed: (){
                         Get.back();
+                        homeCtrl.editCtrl.clear();
+                        homeCtrl.changeTask(null);
                       },
                       icon: const Icon(Icons.close),
                   ),
@@ -85,30 +87,42 @@ class AddDialog extends StatelessWidget {
               ),
             ),
             ...homeCtrl.tasks.map(
-                  (element) => Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 3.0.wp,
-                      horizontal: 5.0.wp,
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(
-                            IconData(
-                                element.icon,
-                                fontFamily: 'MaterialIcons'),
-                            color: HexColor.fromHex(element.color),
+                  (element) => Obx(
+                    () => InkWell(
+                      onTap: () => homeCtrl.changeTask(element),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 3.0.wp,
+                          horizontal: 5.0.wp,
                         ),
-                        SizedBox(
-                          width: 3.0.wp,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  IconData(
+                                      element.icon,
+                                      fontFamily: 'MaterialIcons'),
+                                  color: HexColor.fromHex(element.color),
+                                ),
+                                SizedBox(
+                                  width: 3.0.wp,
+                                ),
+                                Text(
+                                  element.title,
+                                  style: TextStyle(
+                                    fontSize: 12.0.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            if(homeCtrl.task.value == element)
+                              const Icon(Icons.check,color: Colors.blue,)
+                          ],
                         ),
-                        Text(
-                          element.title,
-                          style: TextStyle(
-                            fontSize: 12.0.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
+                      ),
                     ),
                   ),
             )
