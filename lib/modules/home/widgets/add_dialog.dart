ace00d30bf9c9ec9e6316d94965eff53bd0c91c0
Amirhossein_Soleimani <ah.soleimani2@aut.ac.ir>
2023-04-03 19:56:04 +0430

    Add TextFormField toAddDialog in view

--- lib/modules/home/widgets/add_dialog.dart
+++ lib/modules/home/widgets/add_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:task_manager_with_getx/core/utils/extension.dart';
 import 'package:task_manager_with_getx/modules/home/controller.dart';
 
 class AddDialog extends StatelessWidget {
@@ -9,20 +10,68 @@ class AddDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                  onPressed: (){
-                    Get.back();
-                  },
-                  icon: const Icon(Icons.close),
-              )
-            ],
-          )
-        ],
+      body: Form(
+        key: homeCtrl.formKey,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(3.0.wp),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: (){
+                        Get.back();
+                      },
+                      icon: const Icon(Icons.close),
+                  ),
+                  TextButton(
+                      style: ButtonStyle(
+                        overlayColor: MaterialStateProperty.all(Colors.transparent),
+                      ),
+                      onPressed: (){
+
+                      },
+                      child: Text(
+                          'Done',
+                          style: TextStyle(fontSize: 14.0.sp),
+                      ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0.wp),
+              child: Text(
+                'New Task',
+                style: TextStyle(
+                  fontSize: 20.0.sp,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: TextFormField(
+                controller: homeCtrl.editCtrl,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[400]!,
+                    )
+                  )
+                ),
+                autofocus: true,
+                validator: (value){
+                  if(value == null || value.trim().isEmpty){
+                    return 'Please enter your todo item';
+                  }
+                  return null;
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
